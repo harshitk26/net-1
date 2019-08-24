1c9538d169ae00d26467f194bbd17bb9cd405293
Harshit Kachhwaha <33051080+harshitk26@users.noreply.github.com>
2019-08-24 20:41:24 +0530

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,12 @@
+if(request.url === "/index"){
+   fs.readFile("index.html", function (err, data) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      response.end();
+   });
+}
+else{
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+   response.end();
+}
